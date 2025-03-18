a364136ffe522d8ac96c285044ae798545e25e56
pandey104 <88816995+pandey104@users.noreply.github.com>
2025-03-18 18:03:30 +0530

    Update Test5Application.java

--- src/main/java/com/test5/Test5Application.java
+++ src/main/java/com/test5/Test5Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test5Application {
 
 	public static void main(String[] args) {
+		string name = "anjali";
 		SpringApplication.run(Test5Application.class, args);
 	}
 
